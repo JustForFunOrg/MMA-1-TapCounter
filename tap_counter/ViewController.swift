deabb39c2c6a0abb127acd7681ee74e990769805
mmalikov <michail.malykov@gmail.com>
2016-09-12 02:27:34 +0300

    Add mark comment. Remove navigation bar. Add reset and undo button and link them to tap handlers

--- tap_counter/ViewController.swift
+++ tap_counter/ViewController.swift
@@ -10,46 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: Outlets
+    @IBOutlet weak var tapCounterLabel: UILabel!;
+    
+    // MARK: Properties
     var counter = 0;
 
+    // MARK: Init
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        tapCounter.text = String(counter);
-        
-        self.navigationItem.hidesBackButton = true;
-        
-        let resetButton = UIBarButtonItem(title: "Reset", style: UIBarButtonItemStyle.Bordered, target: self, action: Selector("resetButton"))
-        self.navigationItem.leftBarButtonItem = resetButton;
-        
-        let decreaseByOneButton = UIBarButtonItem(title: "Undo", style: UIBarButtonItemStyle.Bordered, target: self, action: Selector("decreaseByOne"))
-        self.navigationItem.rightBarButtonItem = decreaseByOneButton;
+        tapCounterLabel.text = String(counter);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-    @IBOutlet weak var tapCounter: UILabel!;
 
-    @IBAction func tapButton(sender: UIButton) {
+    
+    // MARK: Actions
+    @IBAction func tapButtonTapped(sender: UIButton) {
         counter += 1;
-        tapCounter.text = String(counter);
+        tapCounterLabel.text = String(counter);
     }
     
-    func resetButton() {
+    @IBAction func resetBarButtonTapped(sender: UIButton) {
         counter = 0;
-        tapCounter.text = String(counter);
+        tapCounterLabel.text = String(counter);
     }
     
-    func decreaseByOne() {
+    @IBAction func undoBarButtonTapped(sender: UIButton) {
         if(counter > 0) {
             counter -= 1;
         }
         
-        tapCounter.text = String(counter);
+        tapCounterLabel.text = String(counter);
     }
 }
 
